030438dff8895fd7f06a7bc3c1f1169ff73dfb7d
Nebojsa Mitrovic <32269043+nebojsa-mitrovic@users.noreply.github.com>
2021-07-09 12:28:56 +0200

    Text Change in Contact Section

--- block-templates/contact-section.php
+++ block-templates/contact-section.php
@@ -22,7 +22,7 @@ $contact_form = get_field('contact_form');
         <div><?php echo $contact_data_opening_hours ?></div>
         <div class="contact__phone"><a href="tel:<?php echo $contact_data_phone ?>"><?php echo $contact_data_phone ?></a></div>
         <div class="contact__address"><?php echo $contact_data_address ?></div>
-        <div><a target="_blank" href="<?php echo $contact_data_google_map ?>">Get Directions!</a></div>
+        <div><a target="_blank" href="<?php echo $contact_data_google_map ?>">Anweisungen bekommen!</a></div>
       </div>
       <div class="col-md-6 contact__form"><?php echo $contact_form ?></div>
     </div>
