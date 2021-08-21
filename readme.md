5b8ed8fb677975f814344d3617738de8c31ae187
Nebojsa Mitrovic <32269043+nebojsa-mitrovic@users.noreply.github.com>
2021-08-21 13:51:31 +0200

    Removing search files

--- readme.md
+++ readme.md
@@ -1,6 +1,2 @@
-# Page Builder
-
-## Projects
-* Page builder(develop branch)
-* Page builder for woocommerce (develop_woocommerce branch)
+# wp-dentist
 
--- searchform.php
+++
@@ -1,6 +0,0 @@
-<form class="searchform" role="search" method="get" action="<?php echo home_url('/'); ?>">
-  <input type="search" placeholder="Suche" value="<?php echo get_search_query() ?>" name="s" title="Search" id="example-search-input">
-  <button type="search">
-    <i class="fa fa-search"></i>
-  </button>
-</form>
